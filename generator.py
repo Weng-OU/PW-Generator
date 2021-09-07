c6fb950b091619cc90203523f13404f80bd62e08
Weng-OU <sun021988@gmail.com>
2021-09-07 17:00:03 +0800

    write down todo

--- generator.py
+++ generator.py
@@ -1,7 +1,15 @@
 import os
 import sys
 import hashlib
-
+'''
+keep one short password in mind (ex. "Nj/ ")
+Randomly pick up a salt (input first)
+choose website (input second)
+combine salt and website to generator hash, choose a few numbers as the initial password (ex. first five yards and last five yards)
+combine password in mind and initial password to finial password (output)
+use txt to input second?
+use txt to save output?
+'''
 site_name='google'
 s = hashlib.sha256(site_name.encode('utf-8')).hexdigest()
 print(s)
