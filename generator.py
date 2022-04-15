c42790d6ccba65eb0a7fc2562042bfbbbdc67237
Weng-OU <sun021988@gmail.com>
2022-04-15 11:20:28 +0800

    Add salt selection funtion

--- generator.py
+++ generator.py
@@ -12,8 +12,10 @@ def read_from_file(file_name, folder_name):
 
 def parser_salt_website_argument():
     parser = argparse.ArgumentParser(description='need write here')
-    parser.add_argument('-salt', '-s', type=str, help='Add salt to mess up the encryption result.', default='')
-    parser.add_argument('-url', '-u', type=str, help='Treat the destination URL as an encrypted seed.', default='')
+    parser.add_argument('-salt', '-s', type=str,
+                        help='Add salt to mess up the encryption result.', default='')
+    parser.add_argument('-url', '-u', type=str,
+                        help='Treat the destination URL as an encrypted seed.', default='')
     return parser
 
 
@@ -28,23 +30,26 @@ if __name__ == "__main__":
     if args.salt == '':
         salt_each_lines = read_from_file('salt.txt', pw_source_folder)
     else:
-        salt_each_lines.append('1:{}'.format(args.salt))
+        salt_each_lines.append(args.salt)
 
     if args.url == '':
         website_each_lines = read_from_file('web-site.txt', pw_source_folder)
     else:
-        website_each_lines.append('{}:{}'.format(args.url, args.url))
+        website_each_lines.append('{}:{},1'.format(args.url, args.url))
 
-
-    f_output = open(os.path.join(os.getcwd(), pw_source_folder, 'output.txt'), 'w')
+    f_output = open(os.path.join(
+        os.getcwd(), pw_source_folder, 'output.txt'), 'w')
     for website_line in website_each_lines:
         website = website_line.split(":")[0].strip()
-        url = website_line.split(':')[1].strip()
-        salt = salt_each_lines[0].split(':')[1].strip()
-        hash_pw = hashlib.sha256(salt.encode('utf-8') + url.encode('utf-8')).hexdigest()
+        url = website_line.split(':')[1].strip().split(',')[0].strip()
+        salt_num = website_line.split(':')[1].strip().split(',')[1].strip()
+        salt = salt_each_lines[int(salt_num)-1]
+        hash_pw = hashlib.sha256(salt.encode(
+            'utf-8') + url.encode('utf-8')).hexdigest()
         init_hash_pw = hash_pw[:4] + hash_pw[60:]
         website_pw_list.append(website)
         website_pw_list.append(init_hash_pw)
     for i in range(0, len(website_pw_list), 2):
-        print('{} : {}'.format(website_pw_list[i], website_pw_list[i + 1]), file=f_output)
+        print('{} : {}'.format(
+            website_pw_list[i], website_pw_list[i + 1]), file=f_output)
     f_output.close()
